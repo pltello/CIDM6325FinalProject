42752e4f778b45ce664a14dd88523de17fa9bee9
Penelope Tello <pltello1@buffs.wtamu.edu>
2023-12-03 23:05:27 -0600

    Deployment Attempt 4

--- Project/wsgi.py
+++ Project/wsgi.py
@@ -7,9 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
-
 from configurations.wsgi import get_wsgi_application
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
